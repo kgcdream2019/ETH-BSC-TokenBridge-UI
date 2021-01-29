7d968324773740dc5ed5042b32d0a192fd2f7a9d
kgcdream2019 <kgc.dream2019@gmail.com>
2021-01-29 22:29:48 +0800

    add initModal

--- packages/react-app/src/components/InitModal.jsx
+++ packages/react-app/src/components/InitModal.jsx
@@ -1,6 +1,7 @@
 import {
   Button,
   Flex,
+  Link,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -31,8 +32,10 @@ export const InitModal = ({ isOpen, onClose }) => {
           mx={{ base: 12, lg: 0 }}
           background="rgb(30, 32, 38)"
         >
-          <ModalHeader p={6}>
-            <Text color="rgb(230,232,236)">Notice</Text>
+          <ModalHeader p={6} textAlign="center">
+            <Text color="rgb(230,232,236)">
+              I hereby confirm I&apos;ve read the following Articles:
+            </Text>
           </ModalHeader>
           <ModalCloseButton
             size="lg"
@@ -42,26 +45,44 @@ export const InitModal = ({ isOpen, onClose }) => {
             // _focus={{ border: 'none', outline: 'none' }}
           />
           <ModalBody px={6} py={0}>
-            <Flex flexDirection="column">
-              <Text mb={2} color="rgb(230,232,236)">
-                I hereby confirm I&apos;ve read the following Articles:
-              </Text>
+            <Flex style={{ textAlign: 'justify' }}>
               <Text mb={2} color="rgb(230,232,236)">
                 1. Fees & Minimum Amounts for JustLiquidity Universal Token
-                Bridge Read HERE https://bit.ly/3t6HZ5R
+                Bridge &nbsp;&nbsp;&nbsp;
+                <Link
+                  style={{}}
+                  color="blue.500"
+                  href="https://bit.ly/3t6HZ5R"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                  style={{ whiteSpace: 'nowrap' }}
+                >
+                  Read HERE
+                </Link>
               </Text>
+            </Flex>
+            <Flex style={{ textAlign: 'justify' }}>
               <Text mb={2} color="rgb(230,232,236)">
-                2. How to get your Token listed on the Token Bridge? Read HERE
-                https://bit.ly/3iV6rmb
+                2. How to get your Token listed on the Token
+                Bridge?&nbsp;&nbsp;&nbsp;
+                <Link
+                  color="blue.500"
+                  href=" https://bit.ly/3iV6rmb"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                  style={{ whiteSpace: 'nowrap' }}
+                >
+                  Read HERE
+                </Link>
               </Text>
             </Flex>
           </ModalBody>
           <ModalFooter p={6}>
             <Flex
               w="100%"
-              justify="space-between"
-              align={{ base: 'stretch', md: 'center' }}
-              direction={{ base: 'column', md: 'row' }}
+              justify="center"
+              align={{ base: 'center', md: 'center' }}
+              direction={{ base: 'row', md: 'row' }}
             >
               <Button
                 px={12}
