868391dabda7a5146d61064f7138e517c63d3b9d
kgcdream2019 <kgc.dream2019@gmail.com>
2021-04-04 22:37:27 +0800

    Add maintanenance mode

--- packages/react-app/src/Routes.jsx
+++ packages/react-app/src/Routes.jsx
@@ -3,10 +3,12 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { History } from './pages/History';
 import { Home } from './pages/Home';
+import { Maintenance } from './pages/Maintenance';
 
 export const Routes = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={Maintenance} />
+    {/* <Route exact path="/" component={Home} /> */}
     <Route path="/history" component={History} />
     <Redirect to="/" />
   </Switch>
